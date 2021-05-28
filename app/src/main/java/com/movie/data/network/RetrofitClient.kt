a89994560c9b9f02cf48748b294532232f49dd68
swati <solankiswati71@gmail.com>
2021-05-28 21:14:49 +0530

    API Integration

---
+++ app/src/main/java/com/movie/data/network/RetrofitClient.kt
@@ -0,0 +1,38 @@
+package com.movie.data.network
+
+import androidx.databinding.library.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    fun <Api> buildApi(
+        api: Class<Api>,
+    ): Api {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(getRetrofitClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(api)
+    }
+
+    private fun getRetrofitClient(): OkHttpClient {
+        val levelType: HttpLoggingInterceptor.Level = if (BuildConfig.DEBUG)
+            HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(levelType)
+        return OkHttpClient.Builder()
+            .connectTimeout(2, TimeUnit.MINUTES)
+            .readTimeout(2, TimeUnit.MINUTES)
+            .writeTimeout(2, TimeUnit.MINUTES)
+            .addInterceptor(logging)
+            .build()
+    }
+}
